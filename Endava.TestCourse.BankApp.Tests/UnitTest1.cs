c8f952ad3a5fa8203afe70319a0fc4ad565ae7f1
Cristian Ceban <cebancristian.94@gmail.com>
2023-10-30 18:08:22 +0200

    Added dependencies for unit tests

---
+++ Endava.TestCourse.BankApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Endava.TestCourse.BankApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
